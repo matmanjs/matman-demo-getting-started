8d27f4dd0fb9fbb6ea11c6a8e8e9207ee1b98f00
linjianghe <linjianghe@tencent.com>
2020-03-14 12:37:57 +0800

    feat: baidu_01

--- baidu_01/src/page_baidu_index/crawlers/get-page-info.js
+++ baidu_01/src/page_baidu_index/crawlers/get-page-info.js
@@ -6,16 +6,38 @@ module.exports = () => {
         userAgent: navigator.userAgent,
         _version: Date.now(),
         searchBtnTxt: document.querySelector('#su').value,
-        footerInfo: getFooterInfo()
+        navInfo: getNavInfo()
     };
 };
 
-function getFooterInfo() {
-    return {
-        // 页面底部的联网备案信息
-        jgwab: {
-            code: $('#jgwab').text(),
-            href: $('#jgwab').attr('href')
-        }
+/**
+ * 获取导航模块的信息
+ */
+function getNavInfo() {
+    const jqContainer = $('#u1');
+    let result = {
+        isExist: !!jqContainer.length
     };
+
+    function getNavData(nameAttr) {
+        const jqItem = $(`a[name=${nameAttr}]`, jqContainer);
+
+        const data = {
+            isExist: !!jqItem.length
+        };
+
+        if (data.isExist) {
+            data.name = jqItem.text();
+            data.href = jqItem.attr('href');
+        }
+
+        return data;
+    }
+
+    if (result.isExist) {
+        result.setting = getNavData('tj_settingicon');
+        result.moreProduct = getNavData('tj_briicon');
+    }
+
+    return result;
 }

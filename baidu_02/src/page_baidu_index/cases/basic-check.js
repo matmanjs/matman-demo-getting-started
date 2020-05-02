218c439fad5cb03fa05a7823181e53935919bb39
linjianghe <linjianghe@tencent.com>
2020-05-02 18:20:35 +0800

    refactor: 升级到 matman 5.0 版本，修改 baidu_02 示例

---
+++ baidu_02/src/page_baidu_index/cases/basic-check.js
@@ -0,0 +1,54 @@
+const matman = require('matman');
+
+function getResult(opts) {
+    return matman
+
+        // 创建 PageDriver，页面驱动控制器
+        .createPageDriver(__filename, { opts })
+
+        // 无头浏览器使用 nightmare.js 框架提供，其底层用的是 Google 的 electron，基于 chromium 内核
+        .useNightmare({ show: opts.show })
+
+        // 设置浏览器参数
+        .setDeviceConfig({
+            'UA': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36 mycustomua',
+            'width': 1250,
+            'height': 400
+        })
+
+        // 设置截屏
+        .setScreenshotConfig(true)
+
+        // 加载页面地址
+        .goto('https://www.baidu.com')
+
+        // 需要等待某些条件达成，才开始运行爬虫脚本
+        .wait('#su')
+
+        // 爬虫脚本的函数，用于获取页面中的数据
+        .evaluate(() => {
+            return {
+                title: document.title,
+                width: window.innerWidth,
+                height: window.innerHeight,
+                userAgent: navigator.userAgent,
+                _version: Date.now(),
+                searchBtnTxt: document.querySelector('#su').value
+            };
+        })
+
+        // 结束，获取结果
+        .end();
+}
+
+module.exports = getResult;
+
+// getResult({ show: true, doNotCloseBrowser: true, useRecorder: false })
+//     .then(function (result) {
+//         console.log(JSON.stringify(result));
+//     })
+//     .catch(function (error) {
+//         console.error('failed:', error);
+//     });
+
+
